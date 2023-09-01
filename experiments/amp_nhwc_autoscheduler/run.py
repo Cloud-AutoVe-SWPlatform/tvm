3710e286b5837c4fbf079db2aaf143f7bcd1ff71
Minhae Ye <wiz.sensitive@rtst.co.kr>
2023-09-01 20:22:48 +0900

    Choose backend

--- experiments/amp_nhwc_autoscheduler/run.py
+++ experiments/amp_nhwc_autoscheduler/run.py
@@ -17,6 +17,7 @@ from tvm.contrib.download import download_testdata
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_enum("backend", "opencv_cpu", ["opencv_cpu", "tvm_gpu"], "Choose backend.")
 flags.DEFINE_enum(
     "model", "mobilenet_v2", ["mobilenet_v2", "resnet50", "efficientnet_v2_s"], "Choose model."
 )
@@ -88,62 +89,68 @@ def main(_):
     torch_model.eval()
 
     dummy_input = torch.randn(data.shape)
-    scripted_torch_model = torch.jit.trace(torch_model, dummy_input).eval()
     input_name = "input_1"
-    shape_list = [(input_name, data.shape)]
-    mod, params = relay.frontend.from_pytorch(scripted_torch_model, shape_list)
-    mod = tvm_amp(mod, params, to_nhwc=True)
-    params = None
-
-    onnx_file = f"{FLAGS.model}.onnx"
-    torch.onnx.export(torch_model, dummy_input, onnx_file, input_names=[input_name])
-    opencv_net = cv2.dnn.readNetFromONNX(onnx_file)
-    opencv_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    opencv_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-    opencv_net.setInput(data)
-
-    # Compile with the history best
-    log_file = f"{FLAGS.model}.json"
-    target = tvm.target.Target("cuda -arch=sm_72", host="llvm -mcpu=carmel")
-    print("Compile...")
-    with auto_scheduler.ApplyHistoryBest(log_file):
-        with tvm.transform.PassContext(
-            opt_level=3, config={"relay.backend.use_auto_scheduler": True}
-        ):
-            lib = relay.build(mod, target=target)
-
-    # Create graph executor
-    dev = str(target)
-    module = graph_executor.GraphModule(lib["default"](dev))
-    dtype = "float32"
-    data_tvm = tvm.nd.array(data.astype(dtype))
-    module.set_input(input_name, data_tvm)
-
-    # Evaluate
-    print("Evaluate inference time cost...")
     repeat = 50
-    ftimer = module.module.time_evaluator("run", dev, repeat=repeat)
-    tvm_prof_res = np.array(ftimer().results) * 1e3  # convert to millisecond
-    print(
-        f"Relay Mean inference time (std dev): {np.mean(tvm_prof_res):.2f} ms ({np.std(tvm_prof_res):.2f} ms)"
-    )
-    module.run()
-    tvm_out = module.get_output(0)
-    top1_tvm = np.argmax(tvm_out.asnumpy())
-    print(f"Relay top-1 id: {top1_tvm}, class name: {synset[top1_tvm]}")
 
-    cv2.setNumThreads(1)
-    opencv_prof_res = []
-    opencv_out = opencv_net.forward()
-    for _ in range(repeat):
-        opencv_net.forward()
-        opencv_prof_res.append(opencv_net.getPerfProfile()[0])
-    opencv_prof_res = np.array(opencv_prof_res) * 1000.0 / cv2.getTickFrequency()
-    print(
-        f"OpenCV Mean inference time (std dev): {np.mean(opencv_prof_res):.2f} ms ({np.std(opencv_prof_res):.2f} ms)"
-    )
-    top1_opencv = np.argmax(opencv_out)
-    print(f"OpenCV top-1 id: {top1_opencv}, class name: {synset[top1_opencv]}")
+    if FLAGS.backend == "opencv_cpu":
+        onnx_file = f"{FLAGS.model}.onnx"
+        torch.onnx.export(
+            torch_model, dummy_input, onnx_file, input_names=[input_name], opset_version=11
+        )
+        opencv_net = cv2.dnn.readNetFromONNX(onnx_file)
+        opencv_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        opencv_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+        opencv_net.setInput(data)
+
+        # Evaluate
+        print("Evaluate inference time cost...")
+        opencv_prof_res = []
+        opencv_out = opencv_net.forward()
+        for _ in range(repeat):
+            opencv_net.forward()
+            opencv_prof_res.append(opencv_net.getPerfProfile()[0])
+        opencv_prof_res = np.array(opencv_prof_res) * 1000.0 / cv2.getTickFrequency()
+        print(
+            f"OpenCV Mean inference time (std dev): {np.mean(opencv_prof_res):.2f} ms ({np.std(opencv_prof_res):.2f} ms)"
+        )
+        top1_opencv = np.argmax(opencv_out)
+        print(f"OpenCV top-1 id: {top1_opencv}, class name: {synset[top1_opencv]}")
+    else:
+        scripted_torch_model = torch.jit.trace(torch_model, dummy_input).eval()
+        shape_list = [(input_name, data.shape)]
+        mod, params = relay.frontend.from_pytorch(scripted_torch_model, shape_list)
+        mod = tvm_amp(mod, params, to_nhwc=True)
+        params = None
+
+        # Compile with the history best
+        log_file = f"{FLAGS.model}.json"
+        target = tvm.target.Target("cuda -arch=sm_72", host="llvm -mcpu=carmel")
+        print("Compile...")
+        with auto_scheduler.ApplyHistoryBest(log_file):
+            with tvm.transform.PassContext(
+                opt_level=3, config={"relay.backend.use_auto_scheduler": True}
+            ):
+                lib = relay.build(mod, target=target)
+
+        # Create graph executor
+        dev = tvm.device(str(target))
+        module = graph_executor.GraphModule(lib["default"](dev))
+        dtype = "float32"
+        data_tvm = tvm.nd.array(data.astype(dtype))
+        module.set_input(input_name, data_tvm)
+
+        # Evaluate
+        print("Evaluate inference time cost...")
+
+        ftimer = module.module.time_evaluator("run", dev, repeat=repeat)
+        tvm_prof_res = np.array(ftimer().results) * 1e3  # convert to millisecond
+        print(
+            f"Relay Mean inference time (std dev): {np.mean(tvm_prof_res):.2f} ms ({np.std(tvm_prof_res):.2f} ms)"
+        )
+        module.run()
+        tvm_out = module.get_output(0)
+        top1_tvm = np.argmax(tvm_out.asnumpy())
+        print(f"Relay top-1 id: {top1_tvm}, class name: {synset[top1_tvm]}")
 
 
 if __name__ == "__main__":
